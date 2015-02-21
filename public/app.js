d9805cee52ccbb285d9bd94e4b0f632d45cb777d
scrivy <scrivinator@gmail.com>
2015-02-20 21:34:44 -0800

    fixes #10, fixes #15

--- public/app.js
+++ public/app.js
@@ -46,99 +46,104 @@ function fadeOutOldMarkers(everyone) {
 
 setInterval(fadeOutOldMarkers, 20000, everyone);
 
-var ws = new WebSocket('ws://' + window.location.host + '/ws');
+var ws;
+function wsInit() {
+	ws = new WebSocket('ws://' + window.location.host + '/ws');
+	console.log(ws);
 
-ws.onmessage = function(event) {
-    try {
-        var message = JSON.parse(event.data);
-    } catch(e) {
-        console.error(e.message);
-        console.error('raw message: ');
-        console.error(event);
-        return;
-    }
-    console.log(message);
+	ws.onmessage = function(event) {
+	    try {
+	        var message = JSON.parse(event.data);
+	    } catch(e) {
+	        console.error(e.message);
+	        console.error('raw message: ');
+	        console.error(event);
+	        return;
+	    }
+	    console.log(message);
 
-  switch(message.action) {
-    case 'allLocations':
-      var locations = message.data.locations;
+	  switch(message.action) {
+	    case 'allLocations':
+	      var locations = message.data.locations;
 
-      Object.keys(locations)
-        .forEach(function(id) {
-          everyone[id] = {
-            marker: L.marker(locations[id].latlng).addTo(map),
-            circle: L.circle(locations[id].latlng, locations[id].accuracy).addTo(map),
-            line: L.polyline([mymarker.getLatLng(), locations[id].latlng]).addTo(map)
-          };
-        })
-      ;
+	      Object.keys(locations)
+	        .forEach(function(id) {
+	          everyone[id] = {
+	            marker: L.marker(locations[id].latlng).addTo(map),
+	            circle: L.circle(locations[id].latlng, locations[id].accuracy).addTo(map),
+	            line: L.polyline([mymarker.getLatLng(), locations[id].latlng]).addTo(map)
+	          };
+	        })
+	      ;
 
-      document.cookie = 'id=' + message.data.id;
+	      document.cookie = 'id=' + message.data.id;
 
-      break;
-    case 'updateLocation':
-      var location = message.data;
-      if (everyone[location.id]) {
-        everyone[location.id].marker
-          .setLatLng(location.latlng)
-          .setOpacity(1)
-        everyone[location.id].circle
-          .setLatLng(location.latlng)
-          .setRadius(location.accuracy)
-          .setStyle({opacity: 0.5})
-        everyone[location.id].line
-          .setLatLngs([
-            mymarker.getLatLng(),
-            location.latlng
-          ])
-        ;
-      } else {
-        everyone[location.id] = {
-          marker: L.marker(location.latlng).addTo(map),
-          circle: L.circle(location.latlng, location.accuracy).addTo(map),
-          line: L.polyline([mymarker.getLatLng(), location.latlng]).addTo(map)
-        };
-      }
+	      break;
+	    case 'updateLocation':
+	      var location = message.data;
+	      if (everyone[location.id]) {
+	        everyone[location.id].marker
+	          .setLatLng(location.latlng)
+	          .setOpacity(1)
+	        everyone[location.id].circle
+	          .setLatLng(location.latlng)
+	          .setRadius(location.accuracy)
+	          .setStyle({opacity: 0.5})
+	        everyone[location.id].line
+	          .setLatLngs([
+	            mymarker.getLatLng(),
+	            location.latlng
+	          ])
+	        ;
+	      } else {
+	        everyone[location.id] = {
+	          marker: L.marker(location.latlng).addTo(map),
+	          circle: L.circle(location.latlng, location.accuracy).addTo(map),
+	          line: L.polyline([mymarker.getLatLng(), location.latlng]).addTo(map)
+	        };
+	      }
 
-      break;
-  }
-};
+	      break;
+	  }
+	};
 
-if (navigator.geolocation) {
-  var geo_options = {
-    enableHighAccuracy: true
-  };
-
-  function geo_success(position) {
-    console.log('got a fix');
-
-    var data = {
-      latlng: [position.coords.latitude, position.coords.longitude],
-      accuracy: Math.ceil(position.coords.accuracy)
-    };
-
-    ws.send(JSON.stringify({ action: 'updateLocation', data: data}));
-
-    mymarker.setLatLng(data.latlng);
-    mycircle
-      .setLatLng(data.latlng)
-      .setRadius(position.coords.accuracy)
-    ;
-
-    Object.keys(everyone)
-      .forEach(function(id) {
-        everyone[id].line
-          .setLatLngs([
-            data.latlng,
-            everyone[id].marker.getLatLng()
-          ])
-      })
-    ;
-  }
-
-  function geo_error() {
-    console.log('geolocation error');
-  }
-
-  navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
 }
+
+navigator.geolocation.watchPosition(geo_success, geo_error, {enableHighAccuracy: true});
+
+function geo_success(position) {
+	if (ws && ws.readyState) {
+		console.log('got a fix');
+
+		var data = {
+			latlng: [position.coords.latitude, position.coords.longitude],
+			accuracy: Math.ceil(position.coords.accuracy)
+		};
+
+		ws.send(JSON.stringify({ action: 'updateLocation', data: data}));
+
+		mymarker.setLatLng(data.latlng);
+		mycircle
+			.setLatLng(data.latlng)
+			.setRadius(position.coords.accuracy)
+		;
+
+		Object.keys(everyone)
+		  .forEach(function(id) {
+		    everyone[id].line
+		      .setLatLngs([
+		        data.latlng,
+		        everyone[id].marker.getLatLng()
+		      ])
+		  })
+		;
+	} else if (!ws) {
+		console.log('got initial fix, initializing websockets');
+		wsInit();
+	}
+}
+
+function geo_error() {
+	console.log('geolocation error');
+}
+
