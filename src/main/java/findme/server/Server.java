9985ab4b572b9bd86b88743360ad962cf3060c4a
Daniel <dscrivano@insikt.com>
2015-02-20 18:15:05 -0800

    fixes #14

--- src/main/java/findme/server/Server.java
+++ src/main/java/findme/server/Server.java
@@ -4,6 +4,9 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
@@ -15,16 +18,36 @@ public final class Server {
 
     public static void main(String[] args) throws Exception {
         // Configure the server.
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        String OS = System.getProperty("os.name");
+        Boolean isLinux = OS.equals("Linux");
+
+        EventLoopGroup bossGroup;
+        EventLoopGroup workerGroup;
+        if (isLinux) {
+            System.out.println("Using Epoll");
+            bossGroup = new EpollEventLoopGroup(1);
+            workerGroup = new EpollEventLoopGroup();
+        } else {
+            bossGroup = new NioEventLoopGroup(1);
+            workerGroup = new NioEventLoopGroup();
+        }
+
         try {
             ServerBootstrap b = new ServerBootstrap();
             b.option(ChannelOption.SO_BACKLOG, 1024);
-            b.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
+            if (isLinux) {
+                b.group(bossGroup, workerGroup)
+                        .channel(EpollServerSocketChannel.class)
 //                    .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(new ServerInitializer());
-
+                        .childHandler(new ServerInitializer());
+            } else {
+                b.group(bossGroup, workerGroup)
+                        .channel(NioServerSocketChannel.class)
+//                    .handler(new LoggingHandler(LogLevel.INFO))
+                        .childHandler(new ServerInitializer());
+            }
+            
             Channel ch = b.bind(8500).sync().channel();
 
             pingAndCleanUpWebSockets();
